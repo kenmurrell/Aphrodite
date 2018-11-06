14a350fc06f8b7762ec54ca28ed6e1f2a39c080e
kenmurrell <kmurrell@markettrack.com>
2018-11-06 17:05:18 -0500

    Building settings menu

--- app/src/main/java/kenmurrell/bfr/MainActivity.java
+++ app/src/main/java/kenmurrell/bfr/MainActivity.java
@@ -15,13 +15,18 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.SmsManager;
 import android.text.InputType;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,6 +35,27 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private final String BF_NUM = "6138858659";
+
+    public static Boolean sendMessage(String msg, String number){
+        if(PhoneNumberUtils.isGlobalPhoneNumber(number)){
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage(number, null, msg, null, null );
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +68,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         //----PERSISTENCE----
         final List<String> attentionMsgList = Arrays.asList(getResources().getStringArray(R.array.attentionMsgList));
         final List<String> foodMsgList = Arrays.asList(getResources().getStringArray(R.array.foodMsgList));
-        SharedPreferences settings = getSharedPreferences("PHONE_NUMBER", 0);
-        if(!settings.contains("NUM")){
-            SharedPreferences.Editor editor = settings.edit();
-            editor.remove("NUM");
-            editor.putString("NUM", BF_NUM);
-            editor.apply();
+
+        SharedPreferences settings = getSharedPreferences("SETTINGS", 0);
+        SharedPreferences.Editor editor = settings.edit();
+        if(!settings.contains("PHONE_NUMBER")){
+            editor.putString("PHONE_NUMBER", "0");
+            Log.d("---EVENT---","Created new phone_number field");
         }
+        if(!settings.contains("RANDOM_MSG")){
+            editor.putBoolean("RANDOM_MSG", false);
+            Log.d("---EVENT---","Created new random_msg field");
+        }
+        if(!settings.contains("SEND_DATA")){
+            editor.putBoolean("SEND_DATA", true);
+            Log.d("---EVENT---","Created new send_data field");
+        }
+        editor.apply();
 
 
         //----PERMISSIONS-----
@@ -67,27 +102,56 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         //-----BUTTONS-----
         FloatingActionButton attentionButton = (FloatingActionButton) findViewById(R.id.attentionButton);
-        FloatingActionButton foodButton = (FloatingActionButton) findViewById(R.id.foodButton) ;
         attentionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int idx = new Random().nextInt(attentionMsgList.size());
                 //sendMessage(attentionMsgList.get(idx));
-                String number = getSharedPreferences("PHONE_NUMBER", 0).getString("NUM", "0");
+                String number = getSharedPreferences("SETTINGS", 0).getString("PHONE_NUMBER", "0");
                 String confirmSent = sendMessage("Give attention pls", number) ? "Sent" : "Not Sent";
                 Snackbar.make(view, confirmSent, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+        FloatingActionButton foodButton = (FloatingActionButton) findViewById(R.id.foodButton) ;
         foodButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int idx = new Random().nextInt(foodMsgList.size());
                 //sendMessage(foodMsgList.get(idx));
-                String number = getSharedPreferences("PHONE_NUMBER", 0).getString("NUM", "0");
+                String number = getSharedPreferences("SETTINGS", 0).getString("PHONE_NUMBER", "0");
                 String confirmSent = sendMessage("FEED ME", number) ? "Sent" : "Not Sent";
                 Snackbar.make(view, confirmSent, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        //------------------------
+        //Error is here! onCheckedChange is not being called
+        //Look at this: https://stackoverflow.com/questions/36868099/make-switch-button-which-is-in-an-alertdialog-do-something-when-its-state-cha/36868526#36868526
+        //------------------------
+        LayoutInflater factory = getLayoutInflater();
+        View optionsView = factory.inflate(R.layout.options_menu, null);
+        SwitchCompat randomSwitch = optionsView.findViewById(R.id.switchRandom);
+        randomSwitch.setChecked(getSharedPreferences("SETTINGS", 0).getBoolean("RANDOM_MSG", false));
+        randomSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = getSharedPreferences("SETTINGS",0).edit();
+                editor.putBoolean("RANDOM_MSG", isChecked);
+                Log.i("--EVENT--","Changed RANDOM_MSG");
+                editor.apply();
+            }
+        });
+        SwitchCompat dataSwitch = optionsView.findViewById(R.id.switchData);
+        dataSwitch.setChecked(getSharedPreferences("SETTINGS", 0).getBoolean("SEND_DATA", true));
+        dataSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = getSharedPreferences("SETTINGS",0).edit();
+                editor.putBoolean("USER_DATA", isChecked);
+                Log.i("--EVENT--","Changed USER_DATA");
+                editor.apply();
             }
         });
 
@@ -102,62 +166,44 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView.setNavigationItemSelectedListener(this);
     }
 
-
-    public static Boolean sendMessage(String msg, String number){
-        if(PhoneNumberUtils.isGlobalPhoneNumber(number)){
-            SmsManager sms = SmsManager.getDefault();
-            sms.sendTextMessage(number, null, msg, null, null );
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_number) {
-            onNavNumber();
-        } else if (id == R.id.nav_settings) {
-
-        } else if (id == R.id.nav_share) {
-
+        switch(id){
+            case R.id.nav_number:
+                onNavNumber();
+                break;
+            case R.id.nav_settings:
+                onNavSettings();
+                break;
+            case R.id.nav_about:
+                onNavAbout();
+                break;
+            case R.id.nav_share:
+                onNavShare();
+                break;
+            default:
+                assert true;
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
-
     public boolean onNavNumber(){
-        final SharedPreferences settings = getSharedPreferences("PHONE_NUMBER", 0);
+        final SharedPreferences settings = getSharedPreferences("SETTINGS", 0);
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Please add a phone number");
         final EditText input = new EditText(this);
         input.setInputType(InputType.TYPE_CLASS_PHONE);
-        input.setText(settings.getString("NUM", "None"));
+        input.setText(settings.getString("PHONE_NUMBER", "None"));
         builder.setView(input);
-
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 SharedPreferences.Editor editor = settings.edit();
-                editor.remove("NUM");
-                editor.putString("NUM", input.getText().toString());
+                editor.putString("PHONE_NUMBER", input.getText().toString());
                 editor.apply();
             }
         });
@@ -168,6 +214,37 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
         builder.show();
+        return true;
+    }
+
+    public boolean onNavSettings(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setCustomTitle(getLayoutInflater().inflate(R.layout.options_menu,null));
+        builder.setNeutralButton("Done", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+        return true;
+    }
+
+    public boolean onNavAbout(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("About the Developer");
+        builder.setMessage("add some stuff here about me later idk");
+        builder.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+        return true;
+    }
+
+    public boolean onNavShare(){
         return true;
     }
 
